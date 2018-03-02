2cf5fc4a44ddd210937a240cccb30ff6e380fcff
Ahmedmahmoud1927 <ahmedhemeda1927@gmail.com>
2018-03-02 20:59:36 +0200

    task1

--- app/src/main/java/com/example/ahmed/mas/HomePage.java
+++ app/src/main/java/com/example/ahmed/mas/HomePage.java
@@ -5,15 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_page);
     }
     public void next(View view){
-        Intent intent = new Intent(this,Main2Activity.class);
+        Intent intent = new Intent(this,LoginForm.class);
         startActivity(intent);
     }
 }
